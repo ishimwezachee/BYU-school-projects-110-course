926e6c8d3f4f5dac5330d3d93ee0a822a6b40989
ishimwezachee <zacheeishimwe@gmail.com>
2021-12-21 15:47:36 +0200

    formating strings

---
+++ formatting_strings.py
@@ -0,0 +1,6 @@
+# program that asks for names and repates it back 
+
+first_name = input('What is your first name? ')
+last_name = input('What is your last name? ')
+
+print(f'Your name is {first_name}, {first_name} {last_name}.')
