ca958b80bda42ebdbf405558e26c09cf36b41754
ishimwezachee <zacheeishimwe@gmail.com>
2021-12-29 23:14:34 +0200

    create a story from user input

---
+++ mad_lab.py
@@ -0,0 +1,16 @@
+print('Please enter the following: ')
+
+adjective = input('adjective: ')
+animal   = input('animal: ')
+verb  = input('verb: ')
+exclamation = input('exclamation: ')
+verb =  input('verb: ')
+verb = input('verb: ')
+
+print('Your story is: ')
+
+print(f"""The other day, I was really in trouble. It all started when I saw a very
+{adjective} {animal} {verb} down the hallway. "{exclamation}!" I yelled. But all
+I could think to do was to {verb} over and over. Miraculously,
+that caused it to stop, but not before it tried to {verb}
+right in front of my family.""") 
